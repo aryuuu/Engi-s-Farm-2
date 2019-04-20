7b0e55d07491138d3e08de55839a587bb3dddbda
marsathoriq <13517071@std.stei.itb.ac.id>
2019-04-20 20:38:08 +0700

    add test

--- test/com/map/MapTest.java
+++ test/com/map/MapTest.java
@@ -1,5 +1,37 @@
+package com.map;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MapTest {
+    Map M = new Map("Map.txt");
+    @Test
+    void getMaxRow() {
+        assertEquals(24, M.getMaxRow());
+    }
 
+    @Test
+    void getMaxCol() {
+        assertEquals(24, M.getMaxCol());
+    }
+
+    @Test
+    void getCell() {
+        assertEquals("Land",M.getCell(2,2).getLegend());
+    }
+    @Test
+    void isValidPos() {
+        assertEquals(true,M.isValidPos(2,2));
+    }
+
+    @Test
+    void isWithinArea() {
+        assertEquals(true,M.isWithinArea(3,3));
+    }
+
+    @Test
+    void isNear() {
+        assertEquals(true, M.isNear(0,22,"Truck"));
+    }
 }
--- test/com/player/LinkedlistTest.java
+++ test/com/player/LinkedlistTest.java
@@ -1,5 +1,41 @@
+package com.player;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LinkedlistTest {
+    Linkedlist<Integer> L = new Linkedlist<Integer>();
+    @Test
+    void size() {
+        L.add(3);
+        L.add(5);
+        assertEquals(2,L.size());
+    }
 
+    @Test
+    void isEmpty() {
+        assertEquals(true,L.isEmpty());
+        L.add(5);
+        assertEquals(false,L.isEmpty());
+    }
+    @Test
+    void add() {
+        L.add(3);
+        assertEquals(3,L.get(1));
+    }
+
+    @Test
+    void remove() {
+        L.add(4);
+        L.add(2);
+        L.remove(1);
+        assertEquals(2,L.get(1));
+    }
+
+    @Test
+    void get() {
+        L.add(1);
+        assertEquals(1,L.get(1));
+    }
 }
